50463bfe88ca2cb293f7e0ecc9161b5f4f9793af
coder8matic <coder8matic@gmail.com>
2022-08-15 13:12:04 +0200

    reorg and dom manipulation improvement

--- assets/js/guessNumber.js
+++ assets/js/guessNumber.js
@@ -6,6 +6,15 @@ function randomNumber(min, max) {
 let secret = randomNumber(1, 100);
 console.log(`Secret is: ${secret}`);
 
+function makeResponse(message, placeholder) {
+    console.log(message);
+    let dom = document;
+    dom.getElementById("message").innerHTML=message;
+    dom.getElementById("input-number").placeholder=placeholder;
+    dom.getElementById("input-number").value=null;
+}
+
+
 let button = document.getElementById('check-button');
 
 button.addEventListener('click', function (event){
@@ -13,28 +22,20 @@ button.addEventListener('click', function (event){
     if (Number.isInteger(guessSecret) == false) {
         var notNumberMessage = `You didn't enter a number. Something went wrong`;
         var notNumberPlaceholder = `Insert a number`;
-        response(notNumberMessage, notNumberPlaceholder);
+        makeResponse(notNumberMessage, notNumberPlaceholder);
     }else {
         if (secret == guessSecret) {
             var guessedMessage = `Congratulations!! <br> You did guess the secret number: <strong>${secret}</strong>!`;
             var guessedPlaceholder = guessSecret;
-            response(guessedMessage, guessedPlaceholder);
+            makeResponse(guessedMessage, guessedPlaceholder);
         } else if (secret > guessSecret){
             var higherMessage = `You didn't guess the secret number. Secret number is <strong>higher<strong>!`;
             var higherPlaceholder = guessSecret;
-            response(higherMessage, higherPlaceholder);
+            makeResponse(higherMessage, higherPlaceholder);
         } else if (secret < guessSecret){
             var lowerMessage = `You didn't guess the secret number. Secret number is <strong>lower<strong>!`;
             var lowerPlaceholder = guessSecret;
-            response(lowerMessage, lowerPlaceholder);
+            makeResponse(lowerMessage, lowerPlaceholder);
         }
     }
 });
-
-
-function response(message, placeholder) {
-    console.log(message);
-    document.getElementById("message").innerHTML=message;
-    document.getElementById("input-number").placeholder=placeholder;
-    document.getElementById("input-number").value=null;
-}
